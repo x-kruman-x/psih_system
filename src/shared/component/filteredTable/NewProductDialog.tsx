c5992af999220d46fc0045dfad7bd70104dfb03d
prskv <doodking.adv@gmail.com>
2025-05-17 02:09:41 +0500

    поддержка нескольких размеров

--- src/shared/component/filteredTable/NewProductDialog.tsx
+++ src/shared/component/filteredTable/NewProductDialog.tsx
@@ -19,17 +19,34 @@ type NewProductFields = {
   description?: string;
   color?: string;
   sex?: string;
-  size?: string;
+  sizes: string[];
 };
 
 const NewProductDialog = memo(function NewProductDialog() {
   const sizes = ["xss", "xs", "s", "m", "l", "xl", "xxl", "xxxl"];
 
-  const { handleSubmit, register, control } = useForm<NewProductFields>();
+  const { handleSubmit, register, control, setValue, getValues, watch } =
+    useForm<NewProductFields>({
+      defaultValues: {
+        sizes: [],
+      },
+    });
 
+  const selectedSizes = watch("sizes");
+
+  const onSizeSelect = (size: string) => {
+    const currentSizes = getValues("sizes") || [];
+
+    const updatedSizes = currentSizes.includes(size)
+      ? currentSizes.filter((s) => s !== size)
+      : [...currentSizes, size];
+
+    setValue("sizes", updatedSizes, { shouldValidate: true });
+  };
   const onSubmit = (data: NewProductFields) => {
     console.log(data);
   };
+
   return (
     <div className="fixed left-1/2 top-1/2 w-[90vw] max-w-[1600px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white border border-black border-solid">
       <div className="border-b border-solid border-black flex items-center px-4 py-2 relative">
@@ -119,27 +136,20 @@ const NewProductDialog = memo(function NewProductDialog() {
             <Typography className="text-center opacity-60 mt-2">
               модификации
             </Typography>
-            <Controller
-              name="size"
-              control={control}
-              render={({ field: { onChange, value } }) => (
-                <div className="flex gap-1 justify-center">
-                  {sizes.map((size) => {
-                    return (
-                      <HoverBorderedEl
-                        key={size}
-                        className="select-none cursor-pointer"
-                        isActive={value === size}
-                        onClick={() => onChange(size)}
-                      >
-                        <Typography>{size.toUpperCase()}</Typography>
-                      </HoverBorderedEl>
-                    );
-                  })}
-                </div>
-              )}
-            />
-
+            <div className="flex gap-1 justify-center">
+              {sizes.map((size) => {
+                return (
+                  <HoverBorderedEl
+                    key={size}
+                    className="select-none cursor-pointer"
+                    isActive={selectedSizes.includes(size)}
+                    onClick={() => onSizeSelect(size)}
+                  >
+                    <Typography>{size.toUpperCase()}</Typography>
+                  </HoverBorderedEl>
+                );
+              })}
+            </div>
             <HoverBorderedEl
               as="button"
               className="mx-auto"
