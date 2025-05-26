d71e2b8e64f4fcd3c6c9924e55ee390eda434ff9
kruch <kruchinkin.s@yandex.ru>
2025-05-27 01:31:13 +0300

    коммит для пуша

--- src/main.tsx
+++ src/main.tsx
@@ -24,35 +24,41 @@ declare module "@tanstack/react-router" {
 }
 
 // TODO!: при неправильном токене все равно пропускает в таблицу, а при обновлении страницы уже на логин перебрасывает
-try {
-  if (localStorage.getItem("access_token")) {
-    await refreshToken();
-    const keysWithInfiniteGcTime = ["auth", "isFilter", 'isFilteredTableFilter'];
-  
-    keysWithInfiniteGcTime.forEach((key) => {
-      queryClient.setQueryDefaults([key], { gcTime: Infinity });
-    });
-  
-    queryClient.setQueryData(["auth"], { isAuth: true });
-    queryClient.setQueryData(["isFilter"], { isFilterOpen: false });
-    queryClient.setQueryData(["isFilteredTableFilter"], {
-      isFilteredTableFilterOpen: false,
-    });
+async () => {
+  try {
+    if (localStorage.getItem("access_token")) {
+      await refreshToken();
+      const keysWithInfiniteGcTime = [
+        "auth",
+        "isFilter",
+        "isFilteredTableFilter",
+      ];
+
+      keysWithInfiniteGcTime.forEach((key) => {
+        queryClient.setQueryDefaults([key], { gcTime: Infinity });
+      });
+
+      queryClient.setQueryData(["auth"], { isAuth: true });
+      queryClient.setQueryData(["isFilter"], { isFilterOpen: false });
+      queryClient.setQueryData(["isFilteredTableFilter"], {
+        isFilteredTableFilterOpen: false,
+      });
+    }
+  } catch (e) {
+    console.log(e);
   }
-} catch(e) {
-  console.log(e)
-}
+};
 
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     // <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        {/* <AuthGuard /> */}
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {/* <AuthGuard /> */}
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
     // </StrictMode>
   );
 }
