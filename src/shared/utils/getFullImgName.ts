0a8c14bd987ae93c3372f2dbfeac927916df106f
kruch <kruchinkin.s@yandex.ru>
2025-06-04 02:26:47 +0300

    фикс изображения на сайте

--- src/shared/utils/getFullImgName.ts
+++ src/shared/utils/getFullImgName.ts
@@ -1,3 +1,3 @@
 export default function getFullImageUrl(imageUrl: string) {
-    return `${import.meta.env.VITE_API_BASE_URL}/${imageUrl}`;
+    return `${'https://psihsystem.com'}/${imageUrl}`;
   };
